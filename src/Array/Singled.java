a82195e6f7446cdb8a666503b54f2566be33ab2b
admin <admin@admin-PC>
2016-09-26 19:18:31 +0530

    TestCase Updated

---
+++ src/Array/Singled.java
@@ -0,0 +1,33 @@
+package Array;
+
+public class Singled {
+
+	public static void main(String args[]) {
+		
+	
+	 int arr[]={1,4,5,6,8,9,10};
+	 
+	 int size=arr.length;
+	 
+	 for(int i=0;i<size;i++) {
+		 
+		 System.out.println("The index value of " +i+ " is " +arr[i]);
+	 }
+	 
+	 int arr1[][] ={{1,2,3,4}, {3,4,5,7}};
+	 
+	 //int size1= arr1.length;
+	 
+	   for(int i=0;i<2;i++) {
+		   
+
+		   for(int j=0;j<4;j++) {
+			   
+			   System.out.print(arr1[i][j]);
+		   }
+		   	   }
+	 
+	 
+	} 
+	 
+}
